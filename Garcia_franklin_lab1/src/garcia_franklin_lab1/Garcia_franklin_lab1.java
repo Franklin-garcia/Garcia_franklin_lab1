32547af77ae0f2d04ed0de0fae488942d9e33edd
franklin garcia <franklin__garcia@unitec.edu>
2017-04-28 14:04:22 -0600

    Agregando

--- Garcia_franklin_lab1/src/garcia_franklin_lab1/Garcia_franklin_lab1.java
+++ Garcia_franklin_lab1/src/garcia_franklin_lab1/Garcia_franklin_lab1.java
@@ -5,6 +5,9 @@
  */
 package garcia_franklin_lab1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Franklin Garcia
@@ -15,7 +18,143 @@ public class Garcia_franklin_lab1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner lectura = new Scanner(System.in);
+        String opcion = "";
+        ArrayList lista = new ArrayList();
+        while (!opcion.equalsIgnoreCase("6")) {
+            System.out.println("Ingrese opcion \n"
+                    + "1.Reclutar \n"
+                    + "2.Modificar \n"
+                    + "3.Dar de alta \n"
+                    + "4.Reportes \n"
+                    + "5.Simulacion \n"
+                    + "6-Salir \n");
+            opcion = lectura.next();
+            switch (opcion) {
+                case "1": {
+                    String opcion_ejercito = "";
+                    while (!opcion_ejercito.equalsIgnoreCase("4")) {
+                        System.out.println("Ingrese opcion de ejrcito \n"
+                                + "1-Rusos \n"
+                                + "2-alemanes \n"
+                                + "Alumnos \n");
+                        opcion_ejercito = lectura.next();
+                        switch (opcion_ejercito) {
+                            case "1": {
+                                String nombre;
+                                int id;
+                                int edad;
+                                int rango;
+                                int resistencia;
+                                String tipo_arma = "";
+                                System.out.println("Ingrese nombre");
+                                nombre = lectura.nextLine();
+                                System.out.println("Ingrese id");
+                                id = lectura.nextInt();
+                                System.out.println("Ingrese edad");
+                                edad = lectura.nextInt();
+                                System.out.println("Ingrese rango");
+                                rango = lectura.nextInt();
+                                System.out.println("Ingrese resistencia");
+                                resistencia = lectura.nextInt();
+                                System.out.println("Elija tipo de arma \n "
+                                        + " 1.AK-47 \n"
+                                        + " 2.Revolver Navant \n "
+                                        + " 3.RPG-7 \n");
+                                String arma = lectura.next();
+                                if (arma.equals("1")) {
+                                    tipo_arma = "AK-47";
+                                } else if (arma.equals("2")) {
+                                    tipo_arma = "Revolver Navant";
+                                } else if (arma.equals("3")) {
+                                    tipo_arma = "RPG-7";
+                                }
+                                lista.add(new rusos(nombre, id, edad, rango, resistencia, tipo_arma));
+                            }
+                            break;
+                            case "2": {
+                                String alias;
+                                int edad;
+                                String casta;
+                                int resistencia;
+                                String tipo_arma = "";
+                                System.out.println("Ingrese alias");
+                                alias = lectura.nextLine();
+                                System.out.println("Ingrese edad");
+                                edad = lectura.nextInt();
+                                System.out.println("Ingrese casta");
+                                casta = lectura.next();
+                                System.out.println("Ingrese resistencia");
+                                resistencia = lectura.nextInt();
+                                System.out.println("Ingrese opcion de arma \n"
+                                        + "1.MP 40 \n "
+                                        + "2.Ametralladora MG42 \n"
+                                        + "3.Pistolas Walther P38 \n");
+                                String opcion_arma = lectura.next();
+                                if (opcion_arma.equals("1")) {
+                                    tipo_arma = "MP 40";
+                                } else if (tipo_arma.equals("2")) {
+                                    tipo_arma = "Ametralladora MG42";
+                                } else if (tipo_arma.equals("3")) {
+                                    tipo_arma = "Pistolas Walther P38";
+                                }
+                                lista.add(new alemanes(alias, edad, casta, resistencia, tipo_arma));
+                            }
+                            break;
+                            case "3": {
+                                String grado_academico;
+                                String apodo;
+                                int numero_cuenta;
+                                int edad;
+                                int resistencia;
+                                String tipo_arma = "";
+                                System.out.println("Ingrese grado academico");
+                                grado_academico = lectura.nextLine();
+                                System.out.println("Ingrese apodo");
+                                apodo = lectura.next();
+                                System.out.println("Ingrese numero de cuenta");
+                                numero_cuenta = lectura.nextInt();
+                                System.out.println("ingrese edad");
+                                edad = lectura.nextInt();
+                                System.out.println("Ingrese resistencia");
+                                resistencia = lectura.nextInt();
+                                System.out.println("Ingrese tipo de arama \n"
+                                        + "1.Discos duros \n"
+                                        + "2.Controles de Wii \n"
+                                        + "3.Laptops \n");
+                                String opcion_arma = lectura.next();
+                                if (opcion_arma.equals("1")) {
+                                    tipo_arma = "Discos duros";
+                                } else if (opcion_arma.equals("2")) {
+                                    tipo_arma = "Controles de Wii";
+                                } else if (opcion_arma.equals("3")) {
+                                    tipo_arma = "Laptops";
+                                }
+                                lista.add(new alumnos(grado_academico, apodo, numero_cuenta, edad, resistencia, tipo_arma));
+                            }
+                            break;
+                        }
+                    }
+                }
+                break;
+                case "2": {
+
+                }
+                break;
+                case "3": {
+
+                }
+                break;
+                case "4": {
+
+                }
+                break;
+                case "5": {
+
+                }
+                break;
+            }
+        }
     }
-    
+
 }
--- Garcia_franklin_lab1/src/garcia_franklin_lab1/alemanes.java
+++ Garcia_franklin_lab1/src/garcia_franklin_lab1/alemanes.java
@@ -9,6 +9,67 @@ package garcia_franklin_lab1;
  *
  * @author Franklin Garcia
  */
-public class alemanes {
-    
+public class alemanes {        
+       private String alias;
+       private int edad;
+       private String casta;
+       private int resistencia;
+       private String tipo_arma;
+
+    public alemanes() {
+    }
+
+    public alemanes(String alias, int edad, String casta, int resistencia, String tipo_arma) {
+        this.alias = alias;
+        this.edad = edad;
+        this.casta = casta;
+        this.resistencia = resistencia;
+        this.tipo_arma = tipo_arma;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCasta() {
+        return casta;
+    }
+
+    public void setCasta(String casta) {
+        this.casta = casta;
+    }
+
+    public int getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public String getTipo_arma() {
+        return tipo_arma;
+    }
+
+    public void setTipo_arma(String tipo_arma) {
+        this.tipo_arma = tipo_arma;
+    }
+
+    @Override
+    public String toString() {
+        return "alemanes{" + "alias=" + alias + ", edad=" + edad + ", casta=" + casta + ", resistencia=" + resistencia + ", tipo_arma=" + tipo_arma + '}';
+    }
+       
 }
